37ead7b97066f96b3ef0dbfc5df186333be093ab
Milos <61538921+MilosM33@users.noreply.github.com>
2021-02-08 23:13:29 +0100

    knižnice

--- Xamarin/OpravaMesta/OpravaMesta.Android/obj/Debug/90/android/environment.arm64-v8a.s
+++ Xamarin/OpravaMesta/OpravaMesta.Android/obj/Debug/90/android/environment.arm64-v8a.s
@@ -73,7 +73,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"7c037027-e2b8-41b9-b063-1aa20be9915c"
+	.asciz	"21549bd3-b32c-4608-8378-7ce4e3f4aea6"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
