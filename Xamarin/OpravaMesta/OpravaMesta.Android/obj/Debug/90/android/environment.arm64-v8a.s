85cd7db6a3a51add08fec23d60ef558222e0fc30
Milos <61538921+MilosM33@users.noreply.github.com>
2021-02-09 21:33:07 +0100

    InternetConnectivityCheck

--- Xamarin/OpravaMesta/OpravaMesta.Android/obj/Debug/90/android/environment.arm64-v8a.s
+++ Xamarin/OpravaMesta/OpravaMesta.Android/obj/Debug/90/android/environment.arm64-v8a.s
@@ -73,7 +73,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"21549bd3-b32c-4608-8378-7ce4e3f4aea6"
+	.asciz	"589adfdf-73d8-4f3f-bd88-c56938d9fb45"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
