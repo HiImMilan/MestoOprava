7baa4df839149804c7c1ba33ed31bea2e42df439
Milos <61538921+MilosM33@users.noreply.github.com>
2021-02-09 21:41:51 +0100

    Pridane System.EventArgs

--- Xamarin/OpravaMesta/OpravaMesta/MainPage.xaml.cs
+++ Xamarin/OpravaMesta/OpravaMesta/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
@@ -12,7 +13,7 @@ namespace OpravaMesta
         public MainPage()
         {
             InitializeComponent();
-
+            
         }
 
         async void LoginClicked(object sender, EventArgs e)
