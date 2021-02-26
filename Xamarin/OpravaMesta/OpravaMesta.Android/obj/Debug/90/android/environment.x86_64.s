b855cbea227fa21ebec74a36863e26d6708edc8d
Milos <61538921+MilosM33@users.noreply.github.com>
2021-02-26 22:15:39 +0100

    pridanie permissii

--- Xamarin/OpravaMesta/OpravaMesta.Android/obj/Debug/90/android/environment.x86_64.s
+++ Xamarin/OpravaMesta/OpravaMesta.Android/obj/Debug/90/android/environment.x86_64.s
@@ -72,7 +72,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"a837abb0-15b0-448b-9938-40ad8958ac6e"
+	.asciz	"2765a5b4-ce06-4e17-8e70-cd58627c3803"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
