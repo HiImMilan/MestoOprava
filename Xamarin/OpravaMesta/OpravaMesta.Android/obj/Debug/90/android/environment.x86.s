dfee738dd0f69aa577063af9e402cbcbc4c42947
Milan Babuljak <milanbabuljak@protonmail.com>
2021-02-27 13:17:54 +0100

    GPS Test Screen

--- Xamarin/OpravaMesta/OpravaMesta.Android/obj/Debug/90/android/environment.x86.s
+++ Xamarin/OpravaMesta/OpravaMesta.Android/obj/Debug/90/android/environment.x86.s
@@ -71,7 +71,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"08a92e51-6d5c-4ad1-8248-7510fb45eb52"
+	.asciz	"637d6cd3-048b-475a-bf25-22d7ec644dcb"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
