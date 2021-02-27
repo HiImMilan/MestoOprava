a9c80d096d531a36d6e54d1af29321ac36646930
Milos <61538921+MilosM33@users.noreply.github.com>
2021-02-27 20:45:29 +0100

    Dokončenie minulého commitu

---
+++ Xamarin/OpravaMesta/OpravaMesta/Utils/HelperMethods.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace OpravaMesta.Utils
+{
+    class HelperMethods
+    {
+        public static async Task<ImageSource> CameraTakePhoto()
+        {
+            var photo = await Plugin.Media.CrossMedia.Current.TakePhotoAsync(
+                new Plugin.Media.Abstractions.StoreCameraMediaOptions() { });
+
+            if (photo != null)
+                return ImageSource.FromStream(() => { return photo.GetStream(); });
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
