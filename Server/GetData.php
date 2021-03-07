0a0757dff536e3cbb1569cf90a097766836f6050
Milos <61538921+MilosM33@users.noreply.github.com>
2021-03-07 11:46:50 +0100

    Oprava projektu, ziaden pawn ale c#

---
+++ Server/GetData.php
@@ -0,0 +1,25 @@
+<?php 
+$lat = $_GET["lat"]; 
+$longitude = $_GET["longy"];
+$db = mysqli_connect("localhost", "root", "", "city"); // DB beží na localhoste len!!!!!
+$sql = "SELECT * FROM problems ORDER BY ((latitude-$lat)*(latitude-$lat)) + ((longitude - $longitude)*(longitude - $longitude)) ASC LIMIT 20"; //https://gist.github.com/statickidz/8a2f0ce3bca9badbf34970b958ef8479
+$result = $db->query($sql);
+
+// https://www.w3schools.com/php/php_mysql_select_orderby.asp
+// https://stackoverflow.com/questions/3563389/php-json-encode-mysql-result
+
+while($row = mysqli_fetch_array($result))
+{
+   $results[] = array(
+      'creator' => $row['creatorUUID'], // NEPOSIELAT!!! BEZPEČNOSTNÁ CHYBA!!!
+      'name' => $row['name'],
+      'description' => $row['descript'],
+      'latitude' => $row['latitude'],
+      'longitude' => $row['longitude'],
+      'URL' => $row['imageURL'],
+   );
+}
+$db->close(); // Uzavre pripojenie na JSON
+$json = json_encode($results);
+echo($json);
+?>
