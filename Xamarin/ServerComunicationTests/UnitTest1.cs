e0adf34f2fee5d9e2dc3d57a40650769ff5ed7a2
Milos <61538921+MilosM33@users.noreply.github.com>
2021-04-10 18:29:28 +0200

    Xunit

---
+++ Xamarin/ServerComunicationTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using NUnit.Framework;
+using OpravaMesta;
+
+namespace ServerComunicationTests
+{
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            
+            Assert.Pass();
+        }
+    }
+}
