47803da8c1342fb63871fe5f6cc7d842a036ccf3
Milos <61538921+MilosM33@users.noreply.github.com>
2021-04-25 21:54:43 +0200

    adas

--- MestoOpravaV2/MestoOpravaV2/ProblemsPage.xaml.cs
+++ MestoOpravaV2/MestoOpravaV2/ProblemsPage.xaml.cs
@@ -16,5 +16,10 @@ namespace MestoOpravaV2
         {
             InitializeComponent();
         }
+
+        private void ImageButton_OnClicked(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
