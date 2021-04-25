5a5e703cc6feac197076aed8c6c731ea570ba315
Milos <61538921+MilosM33@users.noreply.github.com>
2021-04-25 20:47:50 +0200

    gosa

--- MestoOpravaV2/MestoOpravaV2/ProblemsPage.xaml.cs
+++ MestoOpravaV2/MestoOpravaV2/ProblemsPage.xaml.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using MestoOpravaV2.Utils;
-using Xamarin.Essentials;
+
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -13,30 +12,9 @@ namespace MestoOpravaV2
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ProblemsPage : ContentPage
     {
-        /*
-        public GPSManager gpsManager = new GPSManager();
-        private ServerManager serverManager;
         public ProblemsPage()
         {
             InitializeComponent();
-            serverManager = new ServerManager("http://192.168.0.106/MestoOpravaV2/Server/");
         }
-
-        protected override void OnAppearing()
-        {
-            
-        }
-        public async void AddPost(object o, EventArgs args)
-        {
-            await CameraManager.CameraTakePhoto();
-            try
-            {
-                score.Text = serverManager.Add_Post();
-            }
-            catch (PermissionException e)
-            {
-                await DisplayAlert("Chyba", e.Message, "ok");
-            }
-        }*/
     }
 }
