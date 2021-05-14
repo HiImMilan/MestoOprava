dd6f1ad3957a4e594af882fac1df204052134942
Jaro <jarko04@hotmail.sk>
2021-05-14 23:20:38 +0200

    LoginPage and RegisterPage design + Navigation + Prepared code behind

--- MestoOpravaV2/MestoOpravaV2/LoginPage.xaml.cs
+++ MestoOpravaV2/MestoOpravaV2/LoginPage.xaml.cs
@@ -17,5 +17,27 @@ namespace MestoOpravaV2
             InitializeComponent();
             
         }
+
+        async void LoginClicked(object sender, EventArgs e)
+        {
+            if (LoginCheck(EntryEmail.Text, EntryPassword.Text))
+                await Navigation.PushModalAsync(new ProblemsPage());
+            else
+                // tu dajte ten pop-up
+                ;
+        }
+        
+        async void RegisterClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new RegisterPage());
+        }
+
+        bool LoginCheck(string email, string password)
+        {
+            // tu pridajte to, čo to bude porovnávať s dátami v databáze
+            // pokiaľ sa nezhoduje, nech vráti false
+            
+            return true;
+        }
     }
 }
--- MestoOpravaV2/MestoOpravaV2/RegisterPage.xaml.cs
+++ MestoOpravaV2/MestoOpravaV2/RegisterPage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +15,29 @@ namespace MestoOpravaV2
         public RegisterPage()
         {
             InitializeComponent();
+        }
+        async void LoginClicked(object sender, EventArgs e)
+        {
+            await Navigation.PopModalAsync();
+        }
+        
+        async void RegisterClicked(object sender, EventArgs e)
+        {
+            if (SendData(EntryName.Text, EntryEmail.Text, EntryPassword.Text, EntryPasswordConfirmation.Text));
+                await Navigation.PushModalAsync(new ProblemsPage());
+        }
+
+        bool SendData(string name, string email, string password, string passwordConfirmation)
+        {
+            if (password != passwordConfirmation)
+            {
+                // sem hodte pop-up, že sa heslá nezhodovali
+                return false;
+            }
+            
+            // sem dajte niečo, čo tie dáta na server pošle
+            
+            return true;
         }
     }
 }
