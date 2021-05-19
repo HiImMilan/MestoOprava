ccedf0f3d1221efec135c5ca8f91a5c25f82b18e
Jaro32768 <71781871+Jaro32768@users.noreply.github.com>
2021-05-19 10:54:19 +0200

    Added SettingsPage + basics

---
+++ MestoOpravaV2/MestoOpravaV2/SettingsPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MestoOpravaV2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SettingsPage : ContentPage
+    {
+        public SettingsPage()
+        {
+            InitializeComponent();
+        }
+
+        async void NameChangeClicked(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        async void PasswordChangeClicked(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        async void ProfilePictureChangeClicked(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
