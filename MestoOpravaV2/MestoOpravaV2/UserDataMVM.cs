d112f9e719f2f9ebf83e83b6fbb5b0382de74697
Jaro32768 <71781871+Jaro32768@users.noreply.github.com>
2021-05-20 10:55:32 +0200

    small changes in pages + user data binding (name, email, password)

---
+++ MestoOpravaV2/MestoOpravaV2/UserDataMVM.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel;
+using System.Net.Security;
+using System.Runtime.CompilerServices;
+using Xamarin.Essentials;
+
+namespace MestoOpravaV2
+{
+    public class UserDataMVM : INotifyPropertyChanged
+    {
+        private static string name;
+        private static string email;
+        private static string password;
+
+        public string Name
+        {
+            get => name;
+            set
+            {
+                name = value;
+                OnPropertyChanged(nameof(name));
+            }
+        }
+        public string Email
+        {
+            get => email;
+            set
+            {
+                email = value;
+                OnPropertyChanged(nameof(email));
+            }
+        }
+        public string Password
+        {
+            get => password;
+            set
+            {
+                password = value;
+                OnPropertyChanged(nameof(password));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
