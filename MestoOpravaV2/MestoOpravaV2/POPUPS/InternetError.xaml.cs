6f5a587cd11bcb4856dd599dce9eb13f15c2b159
Milos <61538921+MilosM33@users.noreply.github.com>
2021-05-25 22:46:48 +0200

    internet check

--- MestoOpravaV2/MestoOpravaV2/POPUPS/InternetError.xaml.cs
+++ MestoOpravaV2/MestoOpravaV2/POPUPS/InternetError.xaml.cs
@@ -6,17 +6,42 @@ using System.Threading.Tasks;
 using Rg;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using MestoOpravaV2.Utils;
+using Rg.Plugins.Popup.Services;
 
 namespace MestoOpravaV2.POPUPS
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class InternetError : Rg.Plugins.Popup.Pages.PopupPage
     {
-        T
-        public InternetError()
+        Task internetCheck;
+        public InternetError(Action action)
         {
             InitializeComponent();
 
+
+            internetCheck = Task.Run(async () =>
+            {
+                while (true)
+                {
+                    try
+                    {
+                        ServerManager.serverManager.checkDebug();
+                        await PopupNavigation.PopAsync();
+                        Console.WriteLine("Internet success");
+                        action();
+                        break;
+                    }
+                    catch (Exception)
+                    {
+
+                    }
+
+                    Task.Delay(2000);
+                    Console.WriteLine("Waiting");
+
+                }
+            });
 
         }
 
